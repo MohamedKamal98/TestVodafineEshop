2a7e242152cbf391776e3ba172f5a2de0344aa45
MohamedKamal98 <mohamedahmedahmedkamal@gmail.com>
2022-09-02 23:10:07 +0200

    Set up Test Structure

--- src/test/java/PurchaseIphoneProduct/PurchaseIphoneProductRunner.java
+++ src/test/java/PurchaseIphoneProduct/PurchaseIphoneProductRunner.java
@@ -2,8 +2,10 @@ package PurchaseIphoneProduct;
 
 import basePackage.BaseTest;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.Test;
 
-@CucumberOptions (features = )
+@CucumberOptions (glue = "PurchaseIphoneProduct",
+        features = "src/test/java/PurchaseIphoneProduct")
 public class PurchaseIphoneProductRunner extends BaseTest
 {
 
--- src/test/java/PurchaseIphoneProduct/PurchaseIphoneProductSteps.java
+++ src/test/java/PurchaseIphoneProduct/PurchaseIphoneProductSteps.java
@@ -1,4 +1,12 @@
 package PurchaseIphoneProduct;
 
-public class PurchaseIphoneProductSteps {
+import io.cucumber.java.en.Given;
+
+public class PurchaseIphoneProductSteps
+{
+    @Given("^User opens vodafone eshop Home Page$")
+    public void User_opens_vodafone_eshop_Home_Page()
+    {
+        
+    }
 }
